aecb669a5f5dce38a53932a1280d3c8161ad4f85
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 08:29:41 -0600

    Update InvalidOrientationException.java

--- Cram/InvalidOrientationException.java
+++ Cram/InvalidOrientationException.java
@@ -1,10 +1,4 @@
-//Andrew Hudson
-//ash68320
-//700656832
-//a3
 //This program creates the InvalidOrientationException class that creates the exception
-
-
 
 public class InvalidOrientationException extends RuntimeException{
 }
