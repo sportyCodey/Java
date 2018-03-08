f7a795946fec500aec12ff8966b2f72a465be7b1
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 08:29:16 -0600

    Update DominosOverlappingException.java

--- Cram/DominosOverlappingException.java
+++ Cram/DominosOverlappingException.java
@@ -1,11 +1,4 @@
-//Andrew Hudson
-//ash68320
-//700656832
-//a3
 //This program creates the DominosOverlappingException class for the exception
-
-
-
 
 public class DominosOverlappingException extends RuntimeException {
 }
