2e38a9a29847bcb19d1f9ce45c4407b9ff49f72e
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 08:28:33 -0600

    Update NonPositiveIntegerException.java

--- Beetle/NonPositiveIntegerException.java
+++ Beetle/NonPositiveIntegerException.java
@@ -1,17 +1,4 @@
-//Andrew Hudson
-//700656832
-//ash68320
-//a2
 //This program creates the class for the RuntimeException that is used in BeetleGame.java
-
-
-
-
-
-
-
-
-
 
 public class NonPositiveIntegerException extends RuntimeException  {
 } 
