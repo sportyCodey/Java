eb3275b45223199ea459ee31f2640037f0f9e84d
sportyCodey <36137507+sportyCodey@users.noreply.github.com>
2018-03-08 08:30:10 -0600

    Update TooManyOrTooFewRowsColsException.java

--- Cram/TooManyOrTooFewRowsColsException.java
+++ Cram/TooManyOrTooFewRowsColsException.java
@@ -1,10 +1,4 @@
-//Andrew Hudson
-//ash68320
-//700656832
-//a3
 //This program creates the TooManyOrTooFewRowsColsException class which creates the exception
-
-
 
 public class TooManyOrTooFewRowsColsException extends RuntimeException {
 }
