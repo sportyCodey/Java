db6e3b4a124826776d7cddb2fcd4dc0f4b0a8846
Drew <36137507+sportyCodey@users.noreply.github.com>
2018-03-10 09:59:26 -0600

    Update ExceptionTestStudent.java

--- Exercise_Problems1/ExceptionTestStudent.java
+++ Exercise_Problems1/ExceptionTestStudent.java
@@ -1,20 +1,22 @@
+//classes that use and test custom Exception types 
+
 public class ExceptionTestStudent {
   public static void main(String[] args) {
 
-	try{
-      SquareWithException sq1 = new SquareWithException(4);
-      System.out.println("Perimeter for sq1: " + sq1.getPerimeter());
-      System.out.println("Area for sq1: " + sq1.getArea());
+      try {
+      	SquareWithException sq1 = new SquareWithException(4);
+      	System.out.println("Perimeter for sq1: " + sq1.getPerimeter());
+      	System.out.println("Area for sq1: " + sq1.getArea());
 
-      SquareWithException sq2 = new SquareWithException(-3);
-      System.out.println("Perimeter for sq2: " + sq2.getPerimeter());
-      System.out.println("Area for sq2: " + sq2.getArea());
+      	SquareWithException sq2 = new SquareWithException(-3);
+      	System.out.println("Perimeter for sq2: " + sq2.getPerimeter());
+      	System.out.println("Area for sq2: " + sq2.getArea());
 
-  	}
+      }
 
-  	catch(IllegalSquareException ex) {
+      catch(IllegalSquareException ex) {
 	  System.out.println("Error: " + ex.getSides() + " is negative ");
-  	}
+      }
   }
 }
 
